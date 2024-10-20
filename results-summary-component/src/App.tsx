cbc5ac1ca68b0fe966f47ca375d78e955c3b8295
“Bartosz <bartoszdudziak.dev@gmail.com>
2024-10-20 20:12:24 +0200

    Element centered

--- results-summary-component/src/App.tsx
+++ results-summary-component/src/App.tsx
@@ -3,8 +3,10 @@ import './index.css';
 
 function App() {
   return (
-    <main className="min-h-dvh bg-neutral-pale-blue">
-      <ResultsSummary />
+    <main className="min-h-dvh bg-neutral-pale-blue flex justify-center items-center">
+      <div className="w-full">
+        <ResultsSummary />
+      </div>
     </main>
   );
 }
