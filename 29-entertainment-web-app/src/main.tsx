44148bf84a2b1fd7e6934ae436c462053e44673f
“Bartosz <bartoszdudziak.dev@gmail.com>
2024-11-22 20:55:12 +0100

    reactquery devtools removed

--- 29-entertainment-web-app/src/main.tsx
+++ 29-entertainment-web-app/src/main.tsx
@@ -4,7 +4,6 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/routes.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SearchContextProvider } from './context/search/SearchProvider.tsx';
 
 const queryClient = new QueryClient();
@@ -14,7 +13,6 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <SearchContextProvider>
         <RouterProvider router={router} future={{ v7_startTransition: true }} />
-        <ReactQueryDevtools initialIsOpen={false} />
       </SearchContextProvider>
     </QueryClientProvider>
   </StrictMode>,
